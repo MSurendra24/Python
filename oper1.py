1b1e113426fadb4a0fa26f5c60d23ba66098015c
MSurendra24 <mettelasurendra2004@gmail.com>
2025-08-20 11:46:25 +0530

    start

---
+++ oper1.py
@@ -0,0 +1,12 @@
+eids=[12,34,56,78,90]
+print(12 in eids)
+
+uids=101,201,301
+print(100 not in uids)
+
+ename='nm'
+print('n' in ename)
+print('y' not in ename)
+
+ids={101,102,103}
+print(3 in range(4))
