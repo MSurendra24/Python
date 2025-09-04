ab09cc019fc6ae17bdb10a0b7638f59d3c9d14fa
MSurendra24 <mettelasurendra2004@gmail.com>
2025-09-04 11:45:25 +0530

    start

---
+++ modules/one1.py
@@ -0,0 +1,6 @@
+def login(name,password):
+    print('details')
+    return True,'100',[1,2,3]
+a=('nm','sd')
+print(login(*a))
+print(a)
---
+++ modules/two2.py
@@ -0,0 +1,6 @@
+def outerfun():
+    print('outer function')
+def innerfun():
+        print('inner function')
+outerfun()
+innerfun()
