a4e34e00c3ab735f32caa3aa68cc8d4d10537a77
MSurendra24 <mettelasurendra2004@gmail.com>
2025-09-09 12:12:56 +0530

    python

---
+++ modules/five5.py
@@ -0,0 +1,13 @@
+enames=['nm','nm1','nm2','nm']
+# for ename in enames:
+#     print(ename)
+i=0
+while i<=len(enames)-1:
+    print(enames[i])
+    i=i+1
+    
+    
+    
+# employees={12,34,45,56,67,67}
+# for emp in employees:
+#     print(emp)
---
+++ modules/six6.py
@@ -0,0 +1,18 @@
+# enames=['nm','nm1','nm2']
+# enames.append('nm3')
+# print(enames)
+# enames.insert(1,'nm4')
+# print(enames)
+# enames.remove('nm2')
+# print(enames)
+enames={'nm','nm1','nm2',}
+enames.add('nm3')
+print(enames)
+enames.discard('nm2')
+print(enames)
+
+ename=('nm','nm1','nm2')
+# ename.append('nm3')
+# print(ename)
+# ename.insert(1,'nm4')
+# print(ename)
